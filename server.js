1a6143ee757a4f5a74660c9cfa6e2b2ce9e9f76e
ceejayps <ceejayps1308@gmail.com>
2022-02-24 08:37:46 -0500

    initial

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require ("express");
+const { json } = require("express/lib/response");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+const fs = require("fs");
+const { send } = require("process");
+ app.get("/users",(req,res)=> {
+    let user 
+  var userfile =  fs.readFile("./Data/users/users.json", "utf8", async (err, jsonString)  =>  {
+         if (err) {
+             console.log("File read failed:", err);
+             return;
+         }
+         console.log("File data:", jsonString);
+           user = await JSON.parse(jsonString);
+           console.log("this is users ..." +user[1].name);
+
+           for (var i = 0, l = Object.keys(user).length; i < l; i++){
+            if (user[i].id === req.query.id) {
+              res.send( JSON.stringify( user[i]));
+            }
+          }
+          res.send("user not found")
+     });
+     
+       
+      
+
+
+
+})
+
+
+
+
+app.listen(1876)
