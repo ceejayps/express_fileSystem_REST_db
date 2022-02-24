27b0d4aa655310cf4de8175a10f2a0d6b8ebfe39
ceejayps <ceejayps1308@gmail.com>
2022-02-24 09:17:08 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -29,5 +29,30 @@ router.get("/",(req,res)=> {
     });
 
 })
+;
+router.get("/:id",(req,res)=> {
+
+    let user;
+
+    var userfile =  fs.readFile("./Data/users/users.json", "utf8", async (err, jsonString)  =>  {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        console.log("File data:", jsonString);
+          user = await JSON.parse(jsonString);
+          console.log("this is users ..." +user[1].name);
+
+          for (var i = 0, l = Object.keys(user).length; i < l; i++){
+           if (user[i].id === req.params.id) {
+             res.send( JSON.stringify( user[i]));
+           }else{
+            res.send(JSON.stringify(message = "user not found"))
+           }
+         }
+         
+    });
+
+})
 
 module.exports = router
