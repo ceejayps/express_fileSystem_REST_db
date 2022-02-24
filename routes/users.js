2b3d40211b7b65639f325c2d0af17da059450e04
ceejayps <ceejayps1308@gmail.com>
2022-02-24 14:35:59 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -78,6 +78,7 @@ router.post("/",(ctx,res)  =>{
            console.log(isTaken)
             console.log(ctx.body.name)
             if(json[i].name == ctx.body.name){
+                console.log("this is the value of i " + i)
                 console.log(json[i].name)
                 isTaken ++;
 
@@ -86,7 +87,7 @@ router.post("/",(ctx,res)  =>{
 
             }else{
                 console.log(isTaken)
-                if(isTaken == 0){
+                if(isTaken == 0 && i == Count -1 ){
                     console.log(isTaken)
                     json.push({
                         id: userId, 
