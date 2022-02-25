a77d6eb8bc0e8699bbdb8e9e32309469e54f364c
ceejayps <ceejayps1308@gmail.com>
2022-02-24 23:07:01 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const { send } = require("process");
 const admin = { name : "Admin", id: "00", type: "admin", discription: "admin user" }
 const authenticated = { name : "Authenticated", id: "01", type: "user", discription: "default auth user" }
-const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+const months = ["Jan", "Feb", "Far","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 const days = [ "Sunday", "Monday", "Tuedays", "Wednesday","Thursday", "Friday", "Saturday"]
 
 const router = express.Router()
