076b705c9a16452fc6e3916ea6d6e6b3284e67e9
ceejayps <ceejayps1308@gmail.com>
2022-02-25 14:15:08 -0500

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -43,26 +43,10 @@ transactions.post("/",(ctx,res)=>{
 transactions.get("/", async (ctx,res)=>{
 
 
-readFiles(TransactionFolder,)
 
 
-    function readFiles(dirname, onFileContent, onError) {
-        fs.readdir(dirname, function(err, filenames) {
-          if (err) {
-            onError(err);
-            return;
-          }
-          filenames.forEach(function(filename) {
-            fs.readFile(dirname + filename, 'utf-8', function(err, content) {
-              if (err) {
-                onError(err);
-                return;
-              }
-              onFileContent(filename, content);
-            });
-          });
-        });
-      }
+
+    
 //     let data = [];
 //     //console.log(files)
 
