7f1ceac4802b473889eccde5f4a496d1d4c46426
ceejayps <ceejayps1308@gmail.com>
2022-02-24 23:25:29 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { append } = require("express/lib/response");
 const Serverless = require("serverless-http");
 
 const server = express();
@@ -7,5 +8,5 @@ const router = express.Router();
 router.get("/", (ctx, res) => {
     res.json({ well: "port 8080"})
 })
-
+server.use("/.netlify/functions/app",router)
 module.exports.handler = Serverless(server);
