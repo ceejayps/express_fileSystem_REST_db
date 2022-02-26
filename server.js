ffa7ea6c59d77888739a1dd200e31292e36211ab
ceejayps <ceejayps1308@gmail.com>
2022-02-26 07:41:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const { send } = require("process");
 
 
 const app = express();
+mongoose = require("mongoose");
+require("dotenv").config();
+
+//Connect to database
+try {
+  mongoose.connect("mongodb://localhost:27017/usersdb", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log("connected to db");
+} catch (error) {
+  handleError(error);
+}
+process.on('unhandledRejection', error => {
+  console.log('unhandledRejection', error.message);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -15,4 +31,6 @@ const userRouter = require("./routes/users")
 const transactionsRouter = require("./routes/transactions")
 app.use("/users", userRouter);
 app.use("/transactions", transactionsRouter)
-app.listen(process.env.PORT || 1876)
+app.listen(process.env.PORT || 1876,() => {
+    console.log("Server is live on port 1876");
+  })
