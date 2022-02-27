495056e6b20899b7fe375a36dcf76fe093eb6ac3
ceejayps <ceejayps1308@gmail.com>
2022-02-26 23:47:26 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose'),
   _userSchema = mongoose.Schema;
-
 let userSchema = new _userSchema({
   fullName: {
     type: String,
-    required: [true, "smartAss we al know u have a name.. enter it"],
+    required: [true, "smartAss we all know u have a name.. enter it"],
   },
   email: {
     type: String,
     unique: [true, "smartAss we already have this email, use another!"],
     lowercase: true,
     trim: true,
-    required: [true, "smartAss we need a email to know it's you"],
+    required: [true, "smartAss we need a email to know who you are"],
     validate: {
       validator: function (v) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
       },
-      message: '{VALUE} does this looks like an email to you?!'
+      message: 'does {VALUE} looks like an email to you?!'
     }
-
   },
   role: {
     type: String,
@@ -32,8 +30,11 @@ let userSchema = new _userSchema({
   },
   created: {
     type: Date,
-    default: Date.now
+    default:new Date.now()
   }
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+
+
