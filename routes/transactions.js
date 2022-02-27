b71416594c2d57145d22bba6274326d7a1d60550
ceejayps <ceejayps1308@gmail.com>
2022-02-26 22:10:19 -0500

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -37,6 +37,7 @@ transactions.post("/",(ctx,res)=>{
 
 
 transactions.get("/", async (req,res)=>{
+  
      
     let data = [];
     
@@ -58,6 +59,25 @@ transactions.get("/", async (req,res)=>{
 
                res.json( data)
               })
+ })
+
+ transactions.get("/test",(req,res)=>{
+  if (req.headers && req.headers.authorization) {
+    var authorization = req.headers.authorization.split(' ')[1],
+        decoded;
+    try {
+        decoded = jwt.verify(authorization, secret.secretToken);
+    } catch (e) {
+        return res.status(401).send('unauthorized');
+    }
+    var userId = decoded.id;
+    // Fetch the user by id 
+    User.findOne({_id: userId}).then(function(user){
+        // Do something with the user
+        return res.send(200);
+    });
+}
+return res.send(500);
  })
 
 module.exports = transactions
