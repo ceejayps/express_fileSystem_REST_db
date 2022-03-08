7be70ab35317f2bcb031a3aca2803303de88e1dd
ceejayps <ceejayps1308@gmail.com>
2022-03-08 14:00:24 -0500

    Update users.js

--- routes/users/users.js
+++ routes/users/users.js
@@ -123,8 +123,7 @@ router.post('/login',async(ctx,res)=>{
     let email = ctx.body.email;
     let password = ctx.body.password
     const users = []
-    fs.readdir(`Data/users/`, async (err, files) => 
-    {
+    files = fs.readdirSync(`Data/users/`) 
         console.log("firebeore")
         for (let i = 0; i < files.length; i++) {
                users.push( JSON.parse(fs.readFileSync(`Data/users/${files[i]}`, "utf8"))
@@ -137,7 +136,7 @@ router.post('/login',async(ctx,res)=>{
                         let JWT = jwt.sign({id: user.id, name: user.name, email:user.email, role:user.role}, process.env.ACCESS_TOKEN_SECRECT);
                         res.json([JWT,user])
                    } catch (e) {return res.status(500).send({message:""})}
-    })
+    
     console.log("fire")
 })
 
